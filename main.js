4ec2ab5ab0e01b0ba5f8f9690c3dfc8545fce940
nvdinh185 <nvdinh0766777123@gmail.com>
2021-10-14 09:41:38 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// const { createStore } = window.Redux;
+const axios = require('axios');
+
+const loginFormElement = document.querySelector("#loginFormId");
+console.log(loginFormElement);
+if (loginFormElement) {
+    const handleFormSubmit = (e) => {
+        // prevent browser from reloading
+        e.preventDefault();
+
+        const username = loginFormElement.querySelector("#username");
+        const password = loginFormElement.querySelector("#password");
+        // console.log(password.value);
+        if (!username.value || !password.value) return;
+        console.log(username.value, password.value);
+
+        // console.log("Submit", hobbyTextElement.value);
+        // const action = {
+        //     type: 'ADD_HOBBY',
+        //     payload: hobbyTextElement.value
+        // };
+        // store.dispatch(action);
+
+        // reset form
+        loginFormElement.reset();
+    };
+
+    loginFormElement.addEventListener("submit", handleFormSubmit);
+}
